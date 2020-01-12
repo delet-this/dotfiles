eb40420d77f96abd75966074b561923e4f30477a
delet-this <33498862+delet-this@users.noreply.github.com>
2020-01-12 14:44:07 +0200

    update install script

--- .local/bin/installdots.sh
+++ .local/bin/installdots.sh
@@ -3,15 +3,15 @@ git clone --bare --depth 1 --recursive https://github.com/delet-this/dotfiles.gi
 dotfiles () {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
-dotfiles checkout
-if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-else
-  echo "Backing up pre-existing dotfiles.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .dotfiles-backup/$(dirname {}); mv {} .dotfiles-backup/{}'
-fi;
 dotfiles config --local status.showUntrackedFiles no
 dotfiles config --local core.sparseCheckout true
 printf '/*\n!README.*\n' > $HOME/.dotfiles/info/sparse-checkout
 dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles."
+else
+  echo "Backing up pre-existing dotfiles."
+  dotfiles checkout 2>&1 | egrep "^\t" | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .dotfiles-backup/$(dirname {}); mv {} .dotfiles-backup/{}'
+  dotfiles checkout
+  echo "Checked out dotfiles."
+fi
