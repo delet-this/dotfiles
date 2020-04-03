f6391a4a7fa41da1fa96d7f00268848c85d60cad
delet-this <33498862+delet-this@users.noreply.github.com>
2020-04-03 03:32:43 +0300

    polybar mpris

--- polybar-scripts/player-mpris-simple.sh
+++ polybar-scripts/player-mpris-simple.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 player_status=$(playerctl status 2> /dev/null)
+artist=$(playerctl metadata artist 2> /dev/null)
+title=$(playerctl metadata title 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl metadata artist) - $(playerctl metadata title)" | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }'
+    if [ "$artist" != "" ]; then
+	echo "$artist - $title" | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }'
+    else
+	echo "$title" | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }'
+    fi
 elif [ "$player_status" = "Paused" ]; then
     echo "$(playerctl metadata artist) - $(playerctl metadata title)" | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }'
 else
