97e1719cd9af1ee9b27d746715ad015f44e8bae6
delet-this <33498862+delet-this@users.noreply.github.com>
2021-10-05 00:43:01 +0300

    gammastep darkmode

---
+++ gammastep/.config/gammastep/hooks/color_scheme.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$1" = period-changed ]; then
+  case $3 in
+    night)
+      ~/.local/bin/dark-mode.sh dark;;
+    # transition)
+    daytime)
+      ~/.local/bin/dark-mode.sh bright;;
+  esac
+fi
