b52cb67d8bc09b988d65170174cad02ee27008f6
delet-this <33498862+delet-this@users.noreply.github.com>
2021-10-16 01:20:56 +0300

    nvim changes

--- nvim/.config/nvim/plugin/lspsaga.nvim.lua
+++ nvim/.config/nvim/plugin/lspsaga.nvim.lua
@@ -1,13 +1,12 @@
 local saga = require 'lspsaga'
 
-
 saga.init_lsp_saga {
   use_saga_diagnostic_sign = true,
   error_sign = '',
   warn_sign = '',
   hint_sign = '',
   infor_sign = '',
-  dianostic_header_icon = '   ',
+  diagnostic_header_icon = '   ',
   code_action_icon = ' ',
   code_action_prompt = {
     enable = true,
--- nvim/.config/nvim/plugin/nvim-lint.lua
+++ nvim/.config/nvim/plugin/nvim-lint.lua
@@ -2,9 +2,10 @@ require('lint').linters_by_ft = {
   python = {'pylint','flake8'},
 }
 
+-- au BufWritePost <buffer> lua require('lint').try_lint()
 vim.api.nvim_exec([[
   augroup LintOnSave
   au!
-  au BufWritePost <buffer> lua require('lint').try_lint()
+  au BufWritePost * lua require('lint').try_lint()
   augroup END
 ]], false)
