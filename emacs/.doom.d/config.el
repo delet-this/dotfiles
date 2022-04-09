5f3d89fcc8affd470b880a49c054fa5bffa9dde1
delet-this <33498862+delet-this@users.noreply.github.com>
2022-04-09 18:09:12 +0300

    Add doom emacs config

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,86 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-vibrant)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; LSP prefs
+(setq lsp-use-plists t)
+(setq lsp-idle-delay 0.05)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Detect pom.xml before .git etc.
+(after! projectile
+  (add-to-list 'projectile-project-root-files-bottom-up "pom.xml"))
+
+(use-package! evil-snipe
+  :config
+  (setq evil-snipe-override-mode 1)
+  (setq evil-snipe-scope 'buffer)
+  (setq evil-snipe-repeat-scope 'buffer))
+
+(use-package! targets
+  :init
+  ;; (setq targets-user-text-objects '((pipe "|" nil separator)
+  ;;                                   (paren "(" ")" pair :more-keys "b")
+  ;;                                   (bracket "[" "]" pair :more-keys "r")
+  ;;                                   (curly "{" "}" pair :more-keys "c")))
+  :config
+  (targets-setup t)
+
+  (targets-define-composite-to pair-delimiter
+  (("(" ")" pair)
+  ("[" "]" pair)
+  ("{" "}" pair)
+  ("<" ">" pair))
+  :bind t
+  :next-key nil
+  :last-key nil
+  :keys "b"))
